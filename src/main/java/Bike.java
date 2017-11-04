c67b9081f7402b3ab77391a39a1b855562d4e48d
151250128 <151250128@smail.nju.edu.cn>
2017-11-04 15:28:48 +0800

    <dev>:<solve Question 2: Bike>

--- src/main/java/Bike.java
+++ src/main/java/Bike.java
@@ -13,21 +13,16 @@ public class Bike {
         long startTime, endTime;
 
         System.out.println("#1 建表与插入数据");
+        System.out.println("#3 自动补全费用字段，并在用户账户中扣除相应的金额");
         startTime = System.currentTimeMillis();
-//        bike.createTable();
-//        bike.insertData();
+        bike.createTable();
+        bike.insertData();
         endTime = System.currentTimeMillis();
         System.out.println("Time: " + (endTime - startTime) + " ms");
 
         System.out.println("#2 家庭住址添加在用户表");
         startTime = System.currentTimeMillis();
-//        bike.updateUserPlace();
-        endTime = System.currentTimeMillis();
-        System.out.println("Time: " + (endTime - startTime) + " ms");
-
-        System.out.println("#3 自动补全费用字段，并在用户账户中扣除相应的金额");
-        startTime = System.currentTimeMillis();
-        bike.updatePriceAndAmount();
+        bike.updateUserPlace();
         endTime = System.currentTimeMillis();
         System.out.println("Time: " + (endTime - startTime) + " ms");
 
@@ -81,7 +76,7 @@ public class Bike {
                 "  rec_id             INT PRIMARY KEY NOT NULL AUTO_INCREMENT,\n" +
                 "  rec_start_time     DATETIME        NOT NULL,\n" +
                 "  rec_end_time       DATETIME        NOT NULL,\n" +
-                "  rec_price          FLOAT,\n" +
+                "  rec_cost           INT             NOT NULL,\n" +
                 "  rec_user_id        INT             NOT NULL,\n" +
                 "  rec_bike_id        INT             NOT NULL,\n" +
                 "  rec_start_place_id INT             NOT NULL,\n" +
@@ -92,16 +87,7 @@ public class Bike {
                 "  FOREIGN KEY (rec_end_place_id) REFERENCES place (place_id)\n" +
                 ")DEFAULT CHARSET = utf8;";
 
-        String createRepair = "CREATE TABLE repair\n" +
-                "(\n" +
-                "  repair_id       INT PRIMARY KEY NOT NULL AUTO_INCREMENT,\n" +
-                "  repair_bike_id  INT             NOT NULL,\n" +
-                "  repair_place_id INT             NOT NULL,\n" +
-                "  FOREIGN KEY (repair_bike_id) REFERENCES bike (bike_id),\n" +
-                "  FOREIGN KEY (repair_place_id) REFERENCES place (place_id)\n" +
-                ")DEFAULT CHARSET = utf8;";
-
-        String[] statements = {createBike, createPlace, createUser, createRecord, createRepair};
+        String[] statements = {createBike, createPlace, createUser, createRecord};
         util.executeSQLs(statements, con);
     }
 
@@ -138,9 +124,10 @@ public class Bike {
 
         // 插入user
         List<String[]> userList = util.readFile("src/main/resources/user.txt");
-
+        Map<String, String> userAmountMap = new HashMap<>();
         for (String[] user : userList) {
             for (String s : user) {
+                userAmountMap.put(s.split(";")[0], s.split(";")[3]);
                 util.executeSQL("INSERT INTO user (user_id, user_name, user_phone, user_amount) VALUES ("
                         + s.split(";")[0] + ", '" + s.split(";")[1] + "', '" + s.split(";")[2] + "', " + s.split(";")[3] + ");", con);
             }
@@ -151,20 +138,47 @@ public class Bike {
         DateFormat recDateFormat = new SimpleDateFormat("yyyy/MM/dd-HH:mm:ss");
         DateFormat sqlDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
 
-        int i = 0;
         for (String[] record : recordList) {
             for (String s : record) {
                 String[] ssplit = s.split(";");
                 try {
-                    util.executeSQL("INSERT INTO record (rec_user_id, rec_bike_id, rec_start_place_id, rec_start_time, rec_end_place_id, rec_end_time) VALUES ("
-                            + ssplit[0].trim().replaceAll("[^(a-zA-Z0-9\\u4e00-\\u9fa5)]", "") + ", " + ssplit[1] + ", " + placeIdMap.get(ssplit[2]) + ", '" + sqlDateFormat.format(recDateFormat.parse(ssplit[3]))
-                            + "', " + placeIdMap.get(ssplit[4]) + ", '" + sqlDateFormat.format(recDateFormat.parse(ssplit[5])) + "');", con);
+                    //计算本次record_cost
+                    int minutes = (int) (recDateFormat.parse(ssplit[5]).getTime() - recDateFormat.parse(ssplit[3]).getTime()) / 1000;
+                    int rec_cost;
+                    if (minutes <= 1800) {
+                        rec_cost = 1;
+                    } else if (minutes <= 3600) {
+                        rec_cost = 2;
+                    } else if (minutes <= 5400) {
+                        rec_cost = 3;
+                    } else {
+                        rec_cost = 4;
+                    }
+
+                    // 判断user余额是否足够
+                    String user_id = ssplit[0].trim().replaceAll("[^(a-zA-Z0-9\\u4e00-\\u9fa5)]", "");
+                    double user_amount = Double.parseDouble(userAmountMap.get(user_id)) - rec_cost;
+                    if (user_amount > 0) {
+                        // 足够，则插入record
+                        util.executeSQL("INSERT INTO record (rec_cost, rec_user_id, rec_bike_id, rec_start_place_id, " +
+                                "rec_start_time, rec_end_place_id, rec_end_time) VALUES ("
+                                + rec_cost + ", " + user_id + ", " + ssplit[1] + ", " + placeIdMap.get(ssplit[2]) + ", '"
+                                + sqlDateFormat.format(recDateFormat.parse(ssplit[3])) + "', " + placeIdMap.get(ssplit[4]) + ", '"
+                                + sqlDateFormat.format(recDateFormat.parse(ssplit[5])) + "');", con);
+
+                        // 并更新用户余额
+                        util.executeSQL("UPDATE user\n" +
+                                "SET user_amount = " + user_amount
+                                + "WHERE user_id = " + user_id + ";", con);
+                    }
+
                 } catch (ParseException e) {
                     e.printStackTrace();
                 }
 
             }
         }
+
     }
 
     /**
@@ -190,18 +204,29 @@ public class Bike {
     }
 
     /**
-     * 3
-     * 自动补全费用字段，并在用户账户中扣除相应的金额
-     */
-    private void updatePriceAndAmount() {
-
-    }
-
-    /**
      * 4
      * 禁用上一个月内使用超200小时的单车
      */
     private void updateRepairTable() {
+        Calendar c = Calendar.getInstance();
 
+        if (c.get(Calendar.DAY_OF_MONTH) == 1) {
+            String dropStatement = "DROP TABLE IF EXISTS repair;";
+            String createStatement = "CREATE TABLE repair(\n" +
+                    "  SELECT\n" +
+                    "    r.rec_bike_id,\n" +
+                    "    r.rec_end_place_id\n" +
+                    "  FROM\n" +
+                    "    (SELECT\n" +
+                    "       rec_bike_id,\n" +
+                    "       max(rec_end_time)                                        AS last_time,\n" +
+                    "       sum(TIMESTAMPDIFF(SECOND, rec_start_time, rec_end_time)) AS use_time\n" +
+                    "     FROM record\n" +
+                    "     WHERE date_format(rec_start_time, '%Y%m') = date_format(now(), '%Y%m') - 1\n" +
+                    "     GROUP BY rec_bike_id) f, record r\n" +
+                    "  WHERE f.use_time > 200 * 3600 AND f.rec_bike_id = r.rec_bike_id AND r.rec_end_time = f.last_time);";
+            String[] statements = new String[]{dropStatement, createStatement};
+            util.executeSQLs(statements, con);
+        }
     }
 }
